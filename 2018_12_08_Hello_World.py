95ffa2b78d6cd3d3d222a35faecc0665c3532f94
Domen <domen.jemec@gmail.com>
2018-12-08 12:32:01 -0800

    Initiate

---
+++ 2018_12_08_Hello_World.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#Hello world test 2018-12-08
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
---
+++ 2018_12_08_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#2018-12-08 Variables
+
+#Declare a variable & initialize
+
+# re-declaring the variable
+
+# Error: variables of different types cannot be combined
+
+# Global vs local in functions
